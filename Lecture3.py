eb22e56549073a91fc7537aff1940f9b0343600f
AdelinaVilevna <adelina.abdykanyeva@gmail.com>
2020-11-02 23:05:24 +0600

    week2_lecture3

---
+++ Lecture3.py
@@ -0,0 +1,75 @@
+# list_ = list()
+# list_ = []
+# list_ = list("hello")
+# print(type(list_))
+# print(list_)
+# a = "hello"
+# b = a.replace("l", "k")         # nujno dat' obektu imya string
+# print(b)
+
+# a = [1, 2, 3]
+# a.pop()
+# print(a)
+
+# a = [1, 2, 3, 4, [1, 3, 4] ]
+# b = ["a", "b"]
+# # print(len(a))
+# # a[1] = 5
+
+# a.insert(0,0)
+# a = a[2::2]
+# del  a[3]
+# print(a)
+
+# a.extend(b)
+# a = a * 3              # dublirovaniye
+# a[0] = a[-1]*3
+# c = a[0]*3
+# print(c)
+
+                             #Tuples
+# a = ()
+# a = tuple("hello")
+# b = tuple([1, 2, 3])
+# c = (1,)                       # zapyataya doljna byt'
+# # print(type(a))
+# print(a)
+# print(b)
+# print(c)
+# a =()
+# print(dir(a))                    # vytaskivaet komand stroki
+
+a = ("a", "b","c", 1, 2, True, False, None, [1, 2], {"key": "value"})
+# print(a.count("a"))
+# print(a.index(True))
+# print(a.index([1, 2]))
+# a[8] = 5
+# # print(a)
+# del a
+# print(a)
+
+# a = 3      
+# b = 2
+
+# a, b = b, a
+# print("a", a)
+# print("b", b)
+
+# a = 1, "a"
+# print(type(a))
+
+# a = (1, 2, 3, 4 )           # unpacking, when you are trying to get out some tuple out 
+# *b, c, d = a     
+# print(c)
+# print(b)
+# print(d)
+
+# lis = [1, 2, 3]
+# n = len (lis) // 2 
+# x = lis[n:] + lis[:n]
+# print(x)
+
+
+
+
+
